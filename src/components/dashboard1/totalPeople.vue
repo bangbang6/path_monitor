f6c17be376033ad6e7ea283eb3b8a53c32985a6a
bangbang6 <577174667@qq.com>
2022-03-06 19:38:27 +0800

    feat: 所有人数

--- src/components/dashboard1/totalPeople.vue
+++ src/components/dashboard1/totalPeople.vue
@@ -13,17 +13,26 @@
         <div class="center">{{totalNumber}}</div>
         <div class="end">人次</div>
       </div>
+
       <div class="chart">
         <div class="head">
           <div class="title1">
-            <span class="bold">{{diasease}}</span>就医人数统计(人/次)
-          </div>
-          <div class="type">
+            <!-- <span class="bold">{{diasease}}</span> -->
             <el-select v-model="type" placeholder="请选择" size="mini">
               <el-option label="高血压" value="高血压"></el-option>
               <el-option label="糖尿病" value="糖尿病"></el-option>
               <el-option label="心脏病" value="心脏病"></el-option>
             </el-select>
+            <span style="marginLeft:2px">就医人数</span>
+          </div>
+          <div class="type">
+            <el-radio-group v-model="stage" size="mini">
+              <el-radio-button label="日"></el-radio-button>
+              <el-radio-button label="周"></el-radio-button>
+              <el-radio-button label="月"></el-radio-button>
+
+              <el-radio-button label="年"></el-radio-button>
+            </el-radio-group>
           </div>
         </div>
 
@@ -37,6 +46,7 @@
 export default {
   data () {
     return {
+      stage: '日',
       totalNumber: 40037987,
       options: {},
       diasease: '高血压',
@@ -47,25 +57,41 @@ export default {
     type: function () {
       if (this.type === "糖尿病") {
         this.groupData = [2887, 3319, 1401, 3666, 4255, 5120, 2807, 3178, 1600, 3350, 2440, 3840, 2000]
-        this.xAxis = [
-          '8:00',
-          '9:00',
-          '10:00',
-          '11:00',
-          '12:00',
-          '13:00',
-          '14:00',
-          '15:00',
-          '16:00',
-          '17:00',
-          '18:00',
-          '19:00',
-          '20:00',
+        this.totalNumber = 34637152
+        this.diasease = this.type
 
-        ]
         this.options = this.renderOptions(this.groupData, this.xAxis)
       } else {
         this.groupData = [2867, 3219, 1901, 3566, 4355, 5420, 2907, 3278, 2200, 3350, 2140, 3540, 2400]
+        this.diasease = this.type
+        this.totalNumber = 40037987
+
+
+        this.options = this.renderOptions(this.groupData, this.xAxis)
+      }
+    },
+    stage (newStage) {
+      if (newStage === '年') {
+        this.groupData = [2887, 3319, 1401, 3666, 4255, 5120, 2807, 3178, 1600, 3350, 2440, 3840, 2000]
+        this.totalNumber = 1564982216
+        this.diasease = this.type
+        this.xAxis = ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021']
+      } else if (newStage === '月') {
+        this.groupData = [2867, 3219, 1901, 3566, 4355, 5420, 2907, 3278, 2200, 3350, 2140, 3540, 2400]
+
+        this.totalNumber = 16546012
+        this.diasease = this.type
+        this.xAxis = ['四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月', '一月', '二月', '三月']
+      } else if (newStage === '周') {
+        this.groupData = [2887, 3319, 1401, 3666, 4255]
+        this.totalNumber = 1564651
+        this.diasease = this.type
+        this.xAxis = ['第一周', '第二周', '第三周', '第四周', '第五周',]
+
+      } else {
+        this.groupData = [2887, 3319, 1401, 3666, 4255, 5120, 2807, 3178, 1600, 3350, 2440, 3840, 2000]
+        this.totalNumber = 34637152
+        this.diasease = this.type
         this.xAxis = [
           '8:00',
           '9:00',
@@ -82,9 +108,12 @@ export default {
           '20:00',
 
         ]
-        this.options = this.renderOptions(this.groupData, this.xAxis)
       }
+      this.options = this.renderOptions(this.groupData, this.xAxis)
+
+
     }
+
   },
   methods: {
     renderOptions (data, axis) {
@@ -209,7 +238,7 @@ export default {
   .title {
     display: flex;
     width: 100%;
-    height: 10%;
+    height: 8%;
     justify-content: center;
     align-items: center;
     font-size: 16px;
@@ -217,6 +246,7 @@ export default {
     color: #237dbf;
     background: #051d3f;
   }
+
   .main {
     height: 90%;
     background: #093068c4;
@@ -254,7 +284,7 @@ export default {
         height: 30px;
         .title1 {
           color: white;
-          font-size: 12px;
+          font-size: 13px;
           margin-left: 6px;
         }
         .type {
@@ -269,7 +299,7 @@ export default {
   }
 }
 ::v-deep .el-input__inner {
-  width: 100px !important;
+  width: 83px !important;
   background-color: #072c61;
   border-color: #eee;
   color: #eee;
